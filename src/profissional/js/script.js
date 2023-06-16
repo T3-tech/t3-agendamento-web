f9a2c30912ee35714a25c0bdaf4e4ccecf2a3e8e
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-06-15 23:00:04 -0300

    servico e proffisional feito

--- src/profissional/js/script.js
+++ src/profissional/js/script.js
@@ -1,5 +1,101 @@
 const servico = document.getElementById("select-servico");
+const botaoSalvar = document.getElementById("profissional-button");
+const tableProfissional = document.getElementById("table-profissional");
+const closeModal = document.getElementById("closeModal");
+const modal = document.getElementById("modalEdit");
+const servicoEdit = document.getElementById("select-servico-edit");
+//fecha o modal
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    location.reload();
+});
 
+function cadastrar(nome, id_servico) {
+    fetch("http://localhost:8000/profissional", {
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({ nome: nome, id_servico: id_servico }),
+    })
+        .then((res) => console.log(res))
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error:", error));
+    location.reload();
+}
+
+//pega o valor do iput e chama a funcao cadastrar
+botaoSalvar.addEventListener("click", () => {
+    const nome = document.getElementById("profissional").value;
+    const id_servico = Number(servico.options[servico.selectedIndex].value);
+    console.log(nome);
+    console.log(id_servico);
+    if (nome === "" || id_servico === "") {
+        alert("Nome e Serviço não pode ser vazio");
+    } else {
+        cadastrar(nome, id_servico);
+    }
+});
+
+function listarProfissional() {
+    fetch("http://localhost:8000/profissional")
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((profissional) => {
+                tableProfissional.innerHTML += `
+                <tr>
+                    <th scope="row">${profissional.nome}</th>
+                    <th scope="row">${profissional.servico}</th>
+                    <td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target=#modalEdit onClick="editarProfissional(${profissional.id})"><i class="bi bi-pencil-square"></i></button></td>
+                    <td><button class="btn bg-danger" onClick="deletarProfissional(${profissional.id})" ><i class="bi bi-trash3-fill"></i></button></td>
+                </tr>
+
+                `;
+            });
+        })
+        .catch((error) => console.error("Error:", error));
+}
+
+function deletarProfissional(profissionalID) {
+    confirm("Deseja deletar o profissional?");
+    fetch(`http://localhost:8000/profissional/${profissionalID}`, {
+        method: "DELETE",
+    })
+        .then((res) => res.json())
+        .then((data) => {})
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+
+    location.reload();
+}
+
+function editarProfissional(profissionalID) {
+    const botaoSalvar = document.getElementById("saveEdit");
+    botaoSalvar.addEventListener("click", () => {
+        const nome = document.getElementById("profissionalEdit").value;
+        if (nome === "") {
+            alert("Nome do profissional não pode ser vazio");
+            return;
+        }
+        const id_servico = Number(servicoEdit.options[servicoEdit.selectedIndex].value);
+        fetch(`http://localhost:8000/profissional/${profissionalID}`, {
+            method: "PUT",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ 
+                nome: nome,
+                id_servico: id_servico
+             }),
+        });
+        location.reload();
+    });
+}
+
+//lista servico disponives
 function listarServico(elemt) {
     fetch("http://localhost:8000/servico")
         .then((res) => res.json())
@@ -14,3 +110,5 @@ function listarServico(elemt) {
 }
 
 listarServico(servico);
+listarServico(servicoEdit);
+listarProfissional();
--- src/servicos/js/main.js
+++ src/servicos/js/main.js
@@ -1,7 +1,16 @@
 const botaoSalvar = document.getElementById("servico-button");
 const tableServico = document.getElementById("table-servico");
-const modal = document.getElementById("modal");
+const modal = document.getElementById("modalEdit");
+const closeModal = document.getElementById("closeModal");
+const closeModalX = document.getElementById("closeModalX");
 
+//fecha o modal
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    location.reload();
+});
+
+//chama a api para cadastrar servico
 function cadastrar(nome) {
     fetch("http://localhost:8000/servico", {
         headers: {
@@ -17,15 +26,22 @@ function cadastrar(nome) {
     location.reload();
 }
 
+//pega o valor do iput e chama a funcao cadastrar
 botaoSalvar.addEventListener("click", () => {
     const nome = document.getElementById("servico").value;
     console.log(
         "🚀 ~ file: main.js:19 ~ botaoSalvar.addEventListener ~ nome:",
         nome
     );
-    cadastrar(nome);
+
+    if (nome === "") {
+        alert("Nome do serviço não pode ser vazio");
+    } else {
+        cadastrar(nome);
+    }
 });
 
+//lista os servicos
 function listarServico() {
     fetch("http://localhost:8000/servico")
         .then((res) => res.json())
@@ -34,8 +50,8 @@ function listarServico() {
                 tableServico.innerHTML += `
                 <tr>
                 <th scope="row">${servico.nome}</th>
-                  <td><button class="btn btn-warning" id="editar" onclick="editarServico(${servico.id})"><i class="bi bi-pencil-square"></i></button></td>
-                  <td><button class="btn bg-danger" id="deletar" data-bs-toggle="modal" data-bs-target=#modalEdit onclick="deletarServico(${servico.id})" ><i class="bi bi-trash3-fill"></i></button></td>
+                  <td><button class="btn btn-warning" id="editar" data-bs-toggle="modal" data-bs-target=#modalEdit onclick="editarServico(${servico.id})"><i class="bi bi-pencil-square"></i></button></td>
+                  <td><button class="btn bg-danger" id="deletar"  onclick="deletarServico(${servico.id})" ><i class="bi bi-trash3-fill"></i></button></td>
               </tr>
 
                 `;
@@ -44,16 +60,50 @@ function listarServico() {
         .catch((error) => console.error("Error:", error));
 }
 
+//deleta serviço por id
 function deletarServico(servicoId) {
+    confirm("Deseja deletar o serviço?");
     fetch(`http://localhost:8000/servico/${servicoId}`, {
         method: "DELETE",
     })
         .then((res) => res.json())
         .then((data) => {
-            console.log("Serviço deletado:", data);
+            if (data.statusCode == 200) {
+                alert("Serviço deletado com sucesso");
+            }
+            if (data.statusCode == 400) {
+                alert(
+                    "Serviço não pode ser deletado pois pertence a um ou mais profissional"
+                );
+            }
         })
-        .catch((error) => console.error("Error:", error));
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+
     location.reload();
+}
+
+//edita servico por id
+function editarServico(servicoId) {
+    const botaoSalvar = document.getElementById("saveEdit");
+    botaoSalvar.addEventListener("click", () => {
+        const nome = document.getElementById("servicoEdit").value;
+        if (nome === "") {
+            alert("Nome do serviço não pode ser vazio");
+            return;
+        }
+
+        fetch(`http://localhost:8000/servico/${servicoId}`, {
+            method: "PUT",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ nome: nome }),
+        });
+        location.reload();
+    });
 }
 
 listarServico();
